566788d1eae7f85d7b05720f00362b824f0d4229
Rodrigo Figueredo <59716658+rgfigueredo91@users.noreply.github.com>
2020-03-28 20:01:01 -0300

    otros

---
+++ facebook.js
@@ -0,0 +1,18 @@
+
+      <script>
+        window.fbAsyncInit = function() {
+          FB.init({
+            xfbml            : true,
+            version          : 'v6.0'
+          });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/es_ES/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+      }(document, 'script', 'facebook-jssdk'));</script>
+
+     
